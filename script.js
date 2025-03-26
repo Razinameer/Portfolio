19aa00d53e6d494615ab06c10ef03bb102f02374
Razinameer <76422924+Razinameer@users.noreply.github.com>
2025-03-26 13:44:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function cancel(){
 const texts = [
     "DEVELOPER",
     "DESIGNER",
-    "YOUTUBER"
+    "PROGRAMER"
 ]
 let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
@@ -40,20 +40,3 @@ function eraseText(){
 window.onload = typeWriter
 let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
-
-const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
-}
-
-const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
-}
-
-if(darkmode === "active") enableDarkmode()
-
-themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
